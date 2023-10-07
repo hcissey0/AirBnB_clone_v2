2a8f7d89055677fdb66dd66d1187314f8c55919a
Hassan Cissey <hcissey0@gmail.com>
2023-10-07 07:14:50 +0000

    Do Deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,44 +1,52 @@
 #!/usr/bin/python3
 """This script is used as a fabfile"""
 
-import os
+from os.path import exists
 from datetime import datetime
-from fabric.api import local, env, put, run
+from fabric.api import env, put, run
 
 
-def do_pack():
-    """This fucntion is used as the fabric command to pack the files"""
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(date)
-    if not os.path.exists("versions"):
-        if local("mkdir -p versions").failed is True:
-            return None
-    try:
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except Exception as e:
-        return None
+env.hosts = ["54.160.68.240", "34.232.71.122"]
 
 
 def do_deploy(archive_path):
     """This is the deployer"""
 
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
+    file_name = archive_path.split("/")[-1]
+    name = file_name.split(".")[0]
     try:
+        # uploading to the temp folder
         put(archive_path, "/tmp/")
-        file_name = os.path.basename(archive_path)
-        name, ext = os.path.splitext(file_name)
-        run("mkdir -p /data/web_static/releases/{}/".format(
-            name))
+
+        # remove old folder
+        run("rm -fr /data/web_static/releases/{}/".format(name))
+
+        # creating new folder
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+
+        # extracting to the folder
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
             file_name, name))
+
+        # deleting the archive
         run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-                /data/web_startic/releases/{}/".format(name))
+
+        # copying the data to the uncompressed parent folder
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name))
+
+        # deleting the uncompressed folder
+        run("rm -fr /data/web_static/releases/{}/web_static".format(
+            name))
+
+        # removing the old symbolic link
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(name))
+
+        # creating a new symbolic link
+        run("ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(name))
         return True
     except Exception:
         return False
